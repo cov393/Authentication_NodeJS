bebc02745f1a515d020d03b86e75e554987e6a1b
cov393 <covuk393@gmail.com>
2023-02-12 03:49:25 +0000

    POST Method for /students request

--- src/app.js
+++ src/app.js
@@ -1,14 +1,25 @@
 const express = require("express")
+require("./db/conn")
 const app = express()
+const Student = require("./models/students")
 const port=process.env.PORT || 5000
 
+app.use(express.json())
 
 app.get("/",(req,res)=>{
     res.send("hello")
 })
 
-app.post("/students",(req,res)=>{
-    res.send("")
+app.post("/students",async(req,res)=>{
+    try{
+        // console.log(req.body)
+        const user = new Student(req.body) 
+        const createUser = await user.save()
+        res.status(201).send(createUser)
+        // res.send("Hello User")
+    }catch(e){
+        res.status(400).send('Cached Error')
+    }
 })
 
 
